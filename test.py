8f2b5ea55f0b598f0314326d78703e8bff327783
souptik2576 <40964361+souptik2576@users.noreply.github.com>
2022-04-19 13:35:40 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,199 +1,57 @@
-import os
-import random
-
-import pandas as pd
+from flask import Flask, request, jsonify
+from tensorflow import keras
+import cv2
 import numpy as np
-from nltk import word_tokenize 
-from nltk.util import ngrams
-from nltk.corpus import stopwords
-from textblob import TextBlob
-import streamlit as st
+import random
+import os
+from PIL import Image
+import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
-import plotly.express as px
-from transformers import pipeline
-import tokenizers
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import NMF
-from sklearn.pipeline import make_pipeline
-    
-# CSS to inject contained in a string
-hide_dataframe_row_index = """
-            <style>
-            .row_heading.level0 {display:none}
-            .blank {display:none}
-            </style>
-            """
 
-# Inject CSS with Markdown
-st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+from tensorflow.keras.applications import vgg16
+from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras import Sequential, optimizers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
 
-if 'sentiments_run' not in st.session_state:
-    st.session_state['sentiments_run'] = False
-    st.session_state['sentiments'] = ''
+from CV.source.data_preparation import haar_cascade
+#model architecture
+image_size=224
+vgg_conv = vgg16.VGG16(weights='imagenet', include_top=False, input_shape=(image_size, image_size, 3))
+for layer in vgg_conv.layers[:]:
+    layer.trainable = False
+model = Sequential()
+model.add(vgg_conv)
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+model.load_weights("/home/ubuntu/Hackathon-D-Cube/CV/notebooks/vgg_64.h5")
 
+app = Flask(__name__)
 
-st.title('App Sentiment Analysis')
+@app.route("/cv", methods=["PUT","POST","GET"])
+def process_image():
+    file = request.files['image']
+    # Read the image via file.stream
+    img = Image.open(file.stream).convert('RGB')
+    img = np.array(img)
 
- 
-
-st.header("Choose a CSV file ")
-with st.expander("What kind of file do i need to upload ?"):
-            st.write("""
-                A CSV file that contains data with column names app, year, comments.
-            """)  
-uploaded_file = st.file_uploader('File', type = 'csv')
-
-if uploaded_file is not None:
-    try:
-        app_df = pd.read_csv(uploaded_file,index_col=False)
-        ref_columns = pd.DataFrame(app_df.columns,columns = ['headers'])
-    except:
-        st.error(' Error loading file. Is it a CSV file with the mandantory columns :app,year,comments ?')
-        st.stop()
-    if not uploaded_file.name.endswith('.csv') or not set(['app','year','comments']).issubset(list(ref_columns['headers'])):
-        st.error(' Error loading file. Please use a CSV file with the mandantory columns : app,year,comments ?')
-        st.stop()
-    
-    with st.spinner('Finding Sentiments using transformer models...'):
-
-        if st.session_state['sentiments_run'] == False :
-            sentiments = pipeline("sentiment-analysis")(list(app_df['comments']))
-            sentiment_list = []
-            for sentiment in sentiments:
-                sentiment_list.append(sentiment['label'])
-            st.session_state['sentiments_run'] =  True
-            st.session_state['sentiments'] = sentiment_list
-
-    st.subheader('Base Records')
-    col1, col2, col3 = st.columns([10,10,10])
-    col1.metric("No of records", len(app_df))
-    col2.metric("Play store", len(app_df[app_df['app']=='play_store']))
-    col3.metric("App store", len(app_df[app_df['app']=='app_store']))
-
-    st.success('We are done ! Lets analyze this further...')
-    # For testing
-    #st.session_state['sentiments'] = [random.choice(['POSITIVE', 'NEGATIVE']) 
-    #                                 for i in range(len(app_df))]
-    app_df['sentiment'] = st.session_state['sentiments']
-    
-    app_df = app_df[['date', 'ratings', 'comments', 'app', 'year','sentiment']]
-
-    #selected_options = st.multiselect('', app_df['year'].unique())
-    years = app_df['year'].unique()
-    #st.header("Which year you want to analyze sentiments ?")
-    selected_yr_options = st.sidebar.multiselect(
-    "Select year ",
-    years
-    )
-    all_years = st.sidebar.checkbox("Select all years")
-
-    selected_app_options = st.sidebar.multiselect(
-    "Play or App store ",
-    app_df['app'].unique()
-    )
-    all_data = st.sidebar.checkbox("Select all data")
-
-    selected_analysis_type = st.sidebar.multiselect(
-    "Word Freq or Topic Modeling ",
-    ['Word Freq', 'Topic Modeling']
-    )
-    all_analysis = st.sidebar.checkbox("Select all analysis options")
-    
-    if selected_yr_options == [] or all_years :
-        filter_df = app_df[app_df['year'].isin(app_df['year'].unique())]
-    else :
-        filter_df = app_df[app_df['year'].isin(selected_yr_options)]
-    
-    if selected_app_options == [] or all_data :
-        filter_df = filter_df[app_df['app'].isin(app_df['app'].unique())]
+    #pre processing
+    #face=haar_cascade.extract_face(img)
+    img=cv2.resize(img,(224,224))
+    pred=model.predict(np.expand_dims(img,axis=0))
+    print(pred)
+    result=np.argmax(pred)
+    print(result)
+    if result == 0:
+        res="Drunk"
     else:
-        filter_df = filter_df[app_df['app'].isin(selected_app_options)]
+        res="Sober"
+    score=pred[0][result]
+    score=float(np.round(score,2))
+    return jsonify({'state': res, 'confidence':score})
 
-    st.subheader('Filtered Records')
-    col1, col2, col3 = st.columns([10,10,10])
-    col1.metric("No of records", len(filter_df))
-    col2.metric("Play store", len(filter_df[filter_df['app']=='play_store']))
-    col3.metric("App store", len(filter_df[filter_df['app']=='app_store']))
 
-    st.dataframe(filter_df)
-    fig = px.pie(filter_df, values='year', names='sentiment', title='Sentiment breakup')
-    st.plotly_chart(fig)
-
-    
-    neg_comments_df = filter_df[filter_df['sentiment'] == 'NEGATIVE']
-    pos_comments_df = filter_df[filter_df['sentiment'] == 'POSITIVE']
-
-    stoplist = stopwords.words('english')
-
-    def display_word_frequency(df_text_column):
-
-        
-
-        c_vec = CountVectorizer(stop_words=stoplist, ngram_range=(2,3))
-
-        # matrix of ngrams
-        ngrams = c_vec.fit_transform(df_text_column)
-        # count frequency of ngrams
-        count_values = ngrams.toarray().sum(axis=0)
-        # list of ngrams
-        vocab = c_vec.vocabulary_
-        freq_df = pd.DataFrame(sorted([(count_values[i],k) for k,i in vocab.items()], reverse=True)
-                    ).rename(columns={0: 'frequency', 1:'bigram/trigram'})
-        
-
-        return freq_df
-    if 'Word Freq' in selected_analysis_type or all_analysis: #['Word Freq', 'Topic Modeling']
-        with st.expander("About Word Frequency"):
-                st.write("""
-                    Here we are generating most frequent words in a sentiment after 
-                    cleaning up for common *stop* words.
-                """)
-
-        col1, col2 = st.columns([10,10])
-
-        with col1:
-            st.subheader('Positive Comments Word Frequency')
-            st.dataframe(display_word_frequency(pos_comments_df['comments']))
-
-        with col2:
-            st.subheader("Negative Comments Word Frequency")
-            st.dataframe(display_word_frequency(neg_comments_df['comments']))
-        
-    
-    def print_top_words(df_text_column,n_top_words):
-
-        tfidf_vectorizer = TfidfVectorizer(stop_words=stoplist, ngram_range=(2,3))
-        
-        nmf = NMF(n_components=7,max_iter=1000)
-        pipe = make_pipeline(tfidf_vectorizer, nmf)
-        pipe.fit(df_text_column) # change here
-        topic_df = pd.DataFrame(columns = ['Topics'])
-        feature_names = tfidf_vectorizer.get_feature_names()
-
-        for topic_idx, topic in enumerate(nmf.components_):
-            message = ''
-            message = "Topic #%d: " % topic_idx
-            message += " ~ ".join([feature_names[i]
-                                for i in topic.argsort()[:-n_top_words - 1:-1]])
-            #message = message + '\n'
-            print(message.strip())
-            topic_df.loc[len(topic_df)] = [message.lstrip()]
-        return topic_df
-
-    if 'Topic Modeling' in selected_analysis_type or all_analysis: #['Word Freq', 'Topic Modeling']
-        with st.expander("About Topic Modeling"):
-            st.write("""
-                Here we are generating Topic Models using non negative 
-                matrix factorization (NMF).
-            """)    
-
-        col1, col2 = st.columns([10,10])
-
-        with col1:
-            st.subheader("Positive Comments Topic Modeling")
-            st.dataframe(print_top_words(pos_comments_df['comments'],n_top_words=3))
-
-        with col2:
-            st.subheader("Negative Comments Topic Modeling")
-            st.dataframe(print_top_words(neg_comments_df['comments'],n_top_words=5))
+if __name__ == "__main__":
+    app.run(debug=True)
